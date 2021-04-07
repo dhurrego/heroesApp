72f46cfc63e9c856a0e0bda9b4bb70340846e00a
Deivid Urrego <deivid_199615@hotmail.com>
2021-04-07 17:28:50 -0500

    Creacion ImagenPipe Se crea un pipe personalizado para devolver la ruta de la imagén de acuerdo al objeto enviado

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -11,6 +11,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 @NgModule({
@@ -19,7 +20,7 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     BuscarComponent, 
     HeroeComponent, 
     HomeComponent, 
-    ListadoComponent, HeroeTarjetaComponent
+    ListadoComponent, HeroeTarjetaComponent, ImagenPipe
   ],
   imports: [
     CommonModule,
