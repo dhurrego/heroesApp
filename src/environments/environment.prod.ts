8661e8577fabf53e148961647801479203a01bbc
Deivid Urrego <deivid_199615@hotmail.com>
2021-04-09 17:38:59 -0500

    Correcion baseUrl de enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://heroesapp-d4b6d-default-rtdb.firebaseio.com/'
+  baseUrl: 'https://heroesapp-d4b6d-default-rtdb.firebaseio.com'
 };
